b3c9b96e4342842e0255f3453607a439959fe33f
Jake Hollingsworth <jhollin1@uci.edu>
2021-06-05 13:11:15 -0700

    Initial files commit

---
+++ test_beer_recommender.py
@@ -0,0 +1,76 @@
+import unittest
+#from setup import *
+#from utils import *
+
+class Test_Beer_Recommender(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_request_data(self):
+        self.assertEqual(2, 2)
+
+    def test_format_data(self):
+        pass
+
+    def test_write_data(self):
+        pass
+
+    def test_remove_raw_data(self):
+        pass
+
+    def test_read_data(self):
+        pass
+
+    def test_get_numpy_features(self):
+        pass
+
+    def test_train_validation_test_split(self):
+        pass
+
+    def test_read_trained_model(self):
+        pass
+
+    def test_write_trained_model(self):
+        pass
+
+    def test_read_config(self):
+        pass
+
+    def test_normalize_data(self):
+        pass
+
+    def test_unnormalize_data(self):
+        pass
+
+    def test_similarity(self):
+        pass
+
+    def test_find_item_neighborhood(self):
+        pass
+
+    def test_find_dissimilar_items(self):
+        pass
+
+    def test_initialize_system(self):
+        pass
+
+    def test_loss_function(self):
+        pass
+
+    def test_train_model(self):
+        pass
+
+    def test_test_model(self):
+        pass
+
+    def test_cross_validate(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
