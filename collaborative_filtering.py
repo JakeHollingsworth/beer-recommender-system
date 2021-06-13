90ff72b48fd2f76d89e71a21070db07e004a1cc9
sql <jacob.hollingsworth.codes@gmail.com>
2021-06-12 20:59:36 -0700

    Fixed errors in collaborative filtering

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -22,6 +22,12 @@ def initialize_system(n_users,n_items,n_latent):
     theta_init = eps * torch.rand(size=[n_items, n_latent])
     return X_init, theta_init
 
+def get_training_info(X_df):
+    X_train = torch.tensor(training_df['review_overall'].values, dtype=torch.float64)
+    user_indices = torch.tensor(training_df['user_index'].values, dtype=torch.int32)
+    item_indices = torch.tensor(training_df['item_index'].values, dtype=torch.int32)
+    return X_train, user_indices, item_indices
+
 class Matrix_Factorization(torch.nn.Module):
     def __init__(self, X_train, user_indices, item_indices, n_latent, alpha, lmda, epochs):
         '''
@@ -39,17 +45,17 @@ class Matrix_Factorization(torch.nn.Module):
 
         super(Matrix_Factorization, self).__init__()
         eps = .1 # Move to config.
-
-        self._theta = torch.tensor(np.random.uniform(-eps, eps, size=[n_users, n_latent]), requires_grad=True)
-        self._X = torch.tensor(np.random.uniform(-eps, eps, size=[n_items, n_latent]), requires_grad=True)
         self._user_indices = user_indices
         self._item_indices = item_indices
+        n_users, n_items = torch.unique(user_indices).shape[0], torch.unique(item_indices).shape[0]
+        self._theta = torch.tensor(np.random.uniform(-eps, eps, size=[n_users, n_latent]), requires_grad=True)
+        self._X = torch.tensor(np.random.uniform(-eps, eps, size=[n_items, n_latent]), requires_grad=True)
 
-        self._optimizer = torch.optim.Adam([self.X, self.theta], lr=alpha, weight_decay = lmda)
+        self._optimizer = torch.optim.Adam([self._X, self._theta], lr=alpha, weight_decay = lmda)
         self._criterion = torch.nn.MSELoss()
         self._epochs = epochs
 
-        self._data_normalizer = Normalize_Features(X_train)
+        self._data_normalizer = Normalize_Features(X_train, user_indices)
         self._training_set = self._data_normalizer.normalize_data(X_train, user_indices)
 
     def loss_function(self, pred):
@@ -57,10 +63,12 @@ class Matrix_Factorization(torch.nn.Module):
         return self.loss(pred, self._training_set)
 
     def train(self):
-        for e in range(self.epochs):
+        for e in range(self._epochs):
             self._optimizer.zero_grad()
+            theta = torch.index_select(self._theta, 0, self._user_indices)
+            x = torch.index_select(self._X, 0, self._item_indices)
             # Dot's each user vector with each item vector for each reviewed item.
-            prediction = torch.sum(self._theta[item_index] * self._X[user_index], 1)
+            prediction = torch.sum(theta * x, 1)
             loss = self._criterion(prediction, self._training_set)
             loss.backward()
             self._optimizer.step()
@@ -71,10 +79,10 @@ class Matrix_Factorization(torch.nn.Module):
 if __name__ == '__main__':
     config = read_config()
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    training_df = pd.read_csv(config['data_path']+config['data_name'], nrows=100000)
-    X_train = torch.tensor(get_numpy_features(training_df), dtype=torch.float64)
-    matrix_fact_model = Matrix_Factorization(X_train, config['latent_dims'], \
-                          config['learning_rate'], config['regularization_lambda'], \
-                          config['epochs'])
+    training_df = pd.read_csv(config['data_path']+config['data_name'], nrows=10000)
+    X_train, user_indices, item_indices = get_training_info(training_df)
+    matrix_fact_model = Matrix_Factorization(X_train, user_indices, item_indices, \
+                          config['latent_dims'], config['learning_rate'], \
+                          config['regularization_lambda'], config['epochs'])
     matrix_fact_model.to(device)
     matrix_fact_model.train()
