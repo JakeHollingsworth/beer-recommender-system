c5f0412f35e3ef24092a3c3e8b8bcd19927eb919
m-stanfield <mstan44@gmail.com>
2021-06-12 21:48:29 -0700

    Added check to ensure at least one example of each beer is in the training set

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import yaml
 import torch
 import pandas as pd
 import numpy as np
+from collaborative_filtering import *
 
 
 def read_config():
@@ -29,16 +30,21 @@ def get_numpy_features(data_df):
 
 def train_validation_test_split(data_df,train_validation_test_split_ratio,random_state):
     #grabbing one sample for all unique users and add to train data
-    train_df_index = data_df.sample(frac=1,random_state=random_state).drop_duplicates(subset='user_profileName').index
+    train_df_index_user = data_df.sample(frac=1,random_state=random_state).drop_duplicates(subset='user_profileName').index
+    train_df_index_beer = data_df.sample(frac=1,random_state=random_state).drop_duplicates(subset='beer_beerId').index
+    train_df_index = np.union1d(train_df_index_user, train_df_index_beer)
+    #print(train_df_index_user.shape,train_df_index_beer.shape,train_df_index.shape)
     test_df = data_df.drop(index=train_df_index)
-
+    print(train_df_index)
+    print(len(train_df_index_user))
+    print(len(train_df_index_beer))
+    print(len(train_df_index))
     #adding additional samples to reach desired split
     number_additional_samples = int(train_validation_test_split_ratio[0]*len(data_df)-len(train_df_index))
     if number_additional_samples > 0:
-        train_additional_indices = test_df.sample(number_additional_samples,
-                                                    random_state=random_state).index
+        train_additional_indices = test_df.sample(number_additional_samples,  random_state=random_state).index
         test_df.drop(train_additional_indices,inplace=True)
-        train_df_index = train_df_index.append(train_additional_indices)
+        train_df_index = np.union1d(train_df_index, train_additional_indices)
 
     #building dataframes
     train_df = data_df.iloc[train_df_index]
@@ -59,3 +65,13 @@ class Normalize_Features():
 
     def unnormalize_data(self, X, user_indices):
         return X + torch.index_select(self._means, 0, torch.tensor(user_indices, dtype=torch.int32))
+
+if __name__ == "__main__":
+    #user = New_User()
+    #user.load_model('a')
+    dF  = pd.read_csv('data/data.csv',nrows=500)
+    print(dF.head())
+    train, validation, test = train_validation_test_split(dF,[0.7,0.15],4)
+
+    print(len(dF['beer_beerId'].unique())==len(train['beer_beerId'].unique()))
+    print(len(dF['user_profileName'].unique())==len(train['user_profileName'].unique()))
